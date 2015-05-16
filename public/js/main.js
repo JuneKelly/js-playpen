029274de912eb579d2fe0130d102e32cc5fd54ea
Shane Kilkelly <shane@kilkelly.me>
2015-05-16 17:11:00 +0100

    start building an app

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,29 @@
 (function() {
   var app = {
-    dataStore: {}
+    dataStore: {
+      tasks: ['one']
+    }
+  };
+
+  var addTask = function(taskDescription) {
+    app.dataStore.tasks.push(taskDescription);
   };
 
   var _init = function() {
     console.log('>> Hello!');
+
+    // Event handlers
+    var form = d3.select('form#taskForm');
+    form.select('#submit').on('click', function() {
+      console.log('>> clicked');
+      var input = form.select('#taskDescription');
+      var taskDescription = input.property('value');
+      addTask(taskDescription);
+      input.property('value', '');
+    });
+
+
+    window.app = app;
   };
 
   _init();
